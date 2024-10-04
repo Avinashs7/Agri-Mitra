eff1d36b12beee7d7a826316b589c602e03942db
DilipM23 <dilip23062003@gmail.com>
2024-10-04 11:36:38 +0530

    frontend error handling v1

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,15 +3,43 @@ import { Link, useNavigate } from 'react-router-dom'
 import { FaUserAlt, FaLock } from "react-icons/fa";
 import axios from 'axios';
 import farmland from '../images/background.png'
+import {object, string} from 'yup'
+
 const SERVER_URL=import.meta.env.VITE_SERVER_URL;
 
 
 const Login = ({setUser}) => {
     const [loginDetails,setLoginDetails]=useState({});
     const navigate=useNavigate();
+
+    //Function to validate the login details
+    let validationSchema = object({
+        email : string()
+            .email("Enter valid email")
+            .required("Email is required"),
+        password: string()
+            .required("Password is required")
+    });
+
+    const [errors, setErrors] = useState({})
     const loginUser=async(e)=>{
         e.preventDefault();
+
+        //Form Validation
         try{
+            await validationSchema.validate(loginDetails, {abortEarly: false});
+            console.log("Form validated successfully");
+        }
+        catch(error){
+            const newErrors = {}
+            error.inner.forEach(err =>{
+                newErrors[err.path] = err.message;
+            });
+            setErrors(newErrors);
+        }
+        try{
+
+
             const data=await axios.post(`${SERVER_URL}/user/login`,loginDetails)
             const userData=data?.data?.data;
             // console.log(userData?.accessToken)
@@ -21,8 +49,10 @@ const Login = ({setUser}) => {
             localStorage.setItem("accessToken",userData?.accessToken);
             navigate("/");
         }
-        catch(err){
-            console.error(err);
+        catch(err)
+        {
+            console.log(err);
+            
         }
     }
     const updateLoginDetails=(e)=>{
@@ -34,18 +64,20 @@ const Login = ({setUser}) => {
     })
   return (
     <div style={{backgroundImage: `url(${farmland})`}} className='h-[90vh] flex flex-col items-center justify-center bg-repeat-round'>
-        <div className='h-[350px] bg-blue-600/20 border border-gray-300 backdrop-blur-lg px-6 w-96 text-white'>
+        <div className='h-[400px] bg-blue-600/20 border border-gray-300 backdrop-blur-lg px-6 w-96 text-white'>
             {/* Login Form  */}
             <div>
                 <h1 className='text-3xl font-bold pb-6 pt-10 text-center'>Login</h1>
                 <form className='flex flex-col items-center' action=''>
-                    <div className='w-full relative'>
+                    <div className='w-full relative mb-3'>
                         <input type='email' name="email" className='border border-gray-300 w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='email' onChange={updateLoginDetails}/>
-                        <FaUserAlt className='absolute top-[35%] right-3'/>
+                        <FaUserAlt className='absolute top-[30%] right-3'/>
+                        {errors.email && <div className='ml-6 text-xs text-red-600 bg-gray-50 w-28 p-1 rounded-full'>{errors.email}</div>}
                     </div>
-                    <div className='w-full relative'>
+                    <div className='w-full relative mt-4 mb-4'>
                         <input type='password' name='password' className='border border-gray-300 w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='Password' onChange={updateLoginDetails}/>
-                        <FaLock className='absolute top-[35%] right-3'/>
+                        <FaLock className='absolute top-[30%] right-3'/>
+                        {errors.password && <div className='ml-6 text-xs text-red-600 bg-gray-50 w-32 p-1 rounded-full'>{errors.password}</div>}
                     </div>
                     <button className='my-2 py-2 w-full rounded-full border border-blue-300 bg-blue-700 text-white' onClick={loginUser}>Login</button>
                     <span>Do not have an account? <Link to='/signup' className='text-blue-400'>Sign Up</Link></span>
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { MdOutlineMail } from "react-icons/md";
 import { FaUserAlt, FaLock, FaPhoneAlt } from "react-icons/fa";
 import axios from 'axios'
+import {object, string} from 'yup'
 import farmland from '../images/background.png'
 const SERVER_URL=import.meta.env.VITE_SERVER_URL;
 
@@ -10,8 +11,42 @@ const SERVER_URL=import.meta.env.VITE_SERVER_URL;
 const SignUp = () => {
     const navigate=useNavigate();
     const [signUpDetails,setsignUpDetails]=useState({});
+
+    const [errors, setErrors] = useState({})
+
+    let validateSchema = object({
+        email : string()
+            .email("Enter the valid email")
+            .required("This field is required"),
+        phone : string()
+            .matches(/^\d{10}$/, "Phone number must have 10 digits"),
+        password : string()
+            .required("This field is required")
+            .min(8, "Password must have atleast 8 characters")
+            .matches(/[!@#$&*()%^,.{}<>|]/, "Password must contain atleast one special character")
+            .matches(/[0-9]/, "Password must contain atleast one number")
+            .matches(/[A-Z]/,'Password must contain atleast one uppercase letter')
+            .matches(/[a-z]/, "Password must contain atleast one lowercase letter"),
+        firstName: string().required("This field is required")
+    });
+
     const signup=async(e)=>{
         e.preventDefault();
+
+        //Form validation
+        try{
+            await validateSchema.validate(signUpDetails, {abortEarly: false});
+            console.log("Form validated successfully");
+        }
+        catch(error)
+        {
+            const newErrors = {}
+            error.inner.forEach(err =>{
+                newErrors[err.path] = err.message;
+            })
+            setErrors(newErrors);
+        }
+
         const formData=new FormData();
         for(const key in signUpDetails){
             formData.append(key,signUpDetails[key]);
@@ -38,27 +73,26 @@ const SignUp = () => {
                 <h1 className='text-3xl font-bold pb-6 pt-6 text-center text-white'>Sign Up</h1>
                 <form className='flex flex-col items-center' action='' encType="multipart/form-data">
                     <div className='w-full relative'>
-                        <input type='email' name='email' className='border border-gray-300 w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='Email' onChange={updateSignUpDetails}/>
-                        <MdOutlineMail className='absolute top-[35%] right-3 text-white'/>
+                        <input type='email' name='email' className='border border-gray-300  text-white w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='Email' onChange={updateSignUpDetails}/>
+                        <MdOutlineMail className='absolute top-[30%] right-3 text-white'/>
+                        {errors.email && <div className='text-xs text-red-600 bg-gray-50 ml-6 w-32 rounded-full p-1'>{errors.email}</div>}
                     </div>
                     <div className='w-full relative'>
-                        <input type='text' name='firstName' className='border border-gray-300 w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='First Name' onChange={updateSignUpDetails}/>
-                        <FaUserAlt className='absolute top-[35%] right-3 text-white'/>
+                        <input type='text' name='firstName' className='border border-gray-300 text-white w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='First Name' onChange={updateSignUpDetails}/>
+                        <FaUserAlt className='absolute top-[30%] right-3 text-white'/>
+                        {errors.firstname && <div className='text-xs  text-red-600 bg-gray-50 ml-6 w-32 rounded-full p-1'>{errors.firstname}</div>}
                     </div>
                     <div className='w-full relative'>
-                        <input type='text' name='lastName' className='border border-gray-300 w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='Last Name' onChange={updateSignUpDetails}/>
-                        <FaUserAlt className='absolute top-[35%] right-3 text-white'/>
+                        <input type='text' name='lastName' className='border border-gray-300  text-white w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='Last Name' onChange={updateSignUpDetails}/>
+                        <FaUserAlt className='absolute top-[30%] right-3 text-white'/>
                     </div>
                     <div className='w-full relative'>
-                        <input type='number' name='phone' className='border border-gray-300 w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='Phone Number' onChange={updateSignUpDetails}/>
-                        <FaPhoneAlt className='absolute top-[35%] right-3 text-white'/>
+                        <input type='number' name='phone' className='border border-gray-300 text-white w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='Phone Number' onChange={updateSignUpDetails}/>
+                        <FaPhoneAlt className='absolute top-[30%] right-3 text-white'/>
+                        {errors.phone && <div className='text-xs  text-red-600 bg-gray-50 ml-6 w-52 rounded-full p-1'>{errors.phone}</div>}
                     </div>
-                    {/* <div className='w-full relative'>
-                        <input type='file' name='avatar' className='border border-gray-300 w-full rounded-full px-4 py-2 my-2 bg-transparent' onChange={(e)=>setsignUpDetails({...signUpDetails,[e.target.name]:e.target.files[0]})}/>
-                        <FaPhoneAlt className='absolute top-[35%] right-3 text-white'/>
-                    </div> */}
                     <div className='w-full relative'>
-                        <select name='gender' className="border border-gray-300 w-full rounded-2xl px-4 py-2 my-2 bg-transparent" onChange={updateSignUpDetails}>
+                        <select name='gender' className="border border-gray-300 w-full rounded-2xl px-4 py-2 my-2 bg-gray-400 text-white" onChange={updateSignUpDetails}>
                             <option value=''>Gender</option>
                             <option value='M'>Male</option>
                             <option value='F'>Female</option>
@@ -66,11 +100,12 @@ const SignUp = () => {
                         </select><br></br>
                         </div >
                     <div className='w-full relative'>
-                        <input type='password' name='password' className='border border-gray-300 w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='Password' onChange={updateSignUpDetails}/>
-                        <FaLock className='absolute top-[35%] right-3 text-white'/>
+                        <input type='password' name='password' className='border border-gray-300 text-white w-full rounded-full px-4 py-2 my-2 bg-transparent' placeholder='Password' onChange={updateSignUpDetails}/>
+                        <FaLock className='absolute top-[30%] right-3 text-white'/>
+                        {errors.password && <div className='text-xs  text-red-600 bg-gray-50 ml-6 w-64 rounded-full p-1'>{errors.password}</div>}
                     </div>
                     <button className='my-2 py-2 w-full rounded-full bg-blue-700 border border-blue-400 text-white' onClick={signup}>Sign Up</button>
-                    <span>Already have an account? <Link to='/login' className='text-blue-400'>Login</Link></span>
+                    <span className='text-white'>Already have an account? <Link to='/login' className='text-blue-400'>Login</Link></span>
                 </form>
             </div>  
         </div>
